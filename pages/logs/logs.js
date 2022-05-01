602bad750072c91c9de65d73b4eec16cb6c58dee
Richard <renlingcang@outlook.com>
2022-05-02 00:26:13 +0800

    S 格式化文档缩进

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -2,17 +2,17 @@
 const util = require('../../utils/util.js')
 
 Page({
-  data: {
-    logs: []
-  },
-  onLoad() {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return {
-          date: util.formatTime(new Date(log)),
-          timeStamp: log
-        }
-      })
-    })
-  }
+    data: {
+        logs: []
+    },
+    onLoad() {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map(log => {
+                return {
+                    date: util.formatTime(new Date(log)),
+                    timeStamp: log
+                }
+            })
+        })
+    }
 })
