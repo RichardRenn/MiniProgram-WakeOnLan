adc6d9a86325c06b154c903ed298054d9efed96b
Richard <renlingcang@outlook.com>
2022-05-02 00:28:14 +0800

    A 添加魔术包生成等主功能函数

--- pages/wol/main.js
+++ pages/wol/main.js
@@ -76,5 +76,134 @@ Page({
      */
     onShareAppMessage() {
 
+    },
+
+    /**
+     * 获取单例UDPSocket连接
+     */
+    getUDPSocket() {
+        var that = this
+        if (!that.data.udpSocket) {
+            var udpSocket = wx.createUDPSocket()
+            try {
+                udpSocket.bind()
+                that.setData({
+                    udpSocket: udpSocket,
+                })
+            } catch (e) {
+                console.log('获取单例UDPSocket连接错误: ' + e)
+            }
+        }
+        return that.data.udpSocket
+    },
+
+    /**
+     * 从MAC地址生成魔术包
+     */
+    generateMagicPacket(rawMacAddr) {
+        try {
+            console.log('rawMacAddr:' + rawMacAddr)
+            var mac = rawMacAddr.match(/(\w{2})/g)
+            console.log('mac:' + mac)
+            var magicPacket = new Uint8Array(102)
+            for (var n = 0; n < 6; n++) {
+                magicPacket[n] = 255
+            }
+            for (var i = 0; i < mac.length; i++) {
+                var v = parseInt("0x" + mac[i])
+                for (var x = 0; x < 16; x++) {
+                    magicPacket[6 * (x + 1) + i] = v
+                }
+            }
+            console.log('magicPacket:' + magicPacket)
+            return magicPacket
+        } catch (e) {
+            console.log('从MAC地址生成魔术包错误: ' + e)
+            wx.showModal({
+                title: '生成魔术包失败',
+                content: '请检查输入的MAC地址',
+                showCancel: false,
+                confirmColor: app.globalData.green,
+            })
+            return false
+        }
+    },
+
+    /**
+     * 发送魔术包
+     */
+    sendMagicPacket(ip, port, macAddr) {
+        var that = this
+        var udp = that.getUDPSocket()
+        var magicPacket = that.generateMagicPacket(macAddr)
+        if (magicPacket) {
+            udp.send({
+                address: ip || '255.255.255.255',
+                port: parseInt(port) || 9,
+                message: magicPacket
+            })
+            wx.showToast({
+                title: '已发送',
+            })
+        }
+    },
+
+    /**
+     * 唤醒当前条目记录
+     */
+    wakeCurrentItem(e) {
+        var that = this
+        var idx = e.currentTarget.dataset.idx
+        var item = that.data.packets[idx]
+        that.sendMagicPacket(item.ip, item.port, item.macAddr)
+    },
+
+    /**
+     * 新建一条记录
+     */
+    createItem() {
+        wx.navigateTo({
+            url: './detail',
+        })
+    },
+
+    /**
+     * 点击进入某一条记录
+     */
+    tapItem(e) {
+        var idx = e.currentTarget.dataset.idx;
+        wx.navigateTo({
+            url: './detail?idx=' + idx,
+        })
+    },
+
+    /**
+     * 滑动删除操作
+     */
+    slideButtonTa(e) {
+        var that = this
+        var packets = that.data.packets
+        var idx = e.currentTarget.dataset.idx
+        wx.showModal({
+            title: '删除',
+            content: '确定要删除这条记录吗？',
+            confirmColor: app.globalData.red,
+            success(res) {
+                if (res.confirm) {
+                    console.log('slideButtonTap - 删除条目操作: 用户点击确定')
+                    var items = packets.splice(idx, 1)
+                    that.setData({
+                        packets: packets,
+                    })
+                    wx.setStorage({
+                        key: "wolPackets",
+                        data: that.data.packets,
+                    })
+                    console.log('slideButtonTap - 删除成功: idx(' + items[0] + ')')
+                } else if (res.cancel) {
+                    console.log('slideButtonTap - 删除条目操作: 用户点击取消')
+                }
+            }
+        })
     }
 })
